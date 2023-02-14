49559dd1b44701ca4f4657e5c8cbe915f5bd657a
Dmytro Kolomiiets <dmkolomiec@gmail.com>
2023-02-14 16:24:20 +0200

    Update Class1.java

--- src/Class1.java
+++ src/Class1.java
@@ -1,5 +1,6 @@
+// V3
 public class Class1 {
     public static void main(String[] args) {
-        System.out.println("Class1 v2");
+        System.out.println("Class1 v3 made in GitHub");
     }
 }
