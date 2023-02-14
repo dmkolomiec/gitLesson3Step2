ec383829e7ad9fb6f76aafac48bcafa9771d0703
dmKolomiec <dmkolomiec@gmail.com>
2023-02-14 18:42:46 +0200

    changed Class2.java in branch1

--- src/Class3.java
+++ src/Class3.java
@@ -1,5 +1,6 @@
+// branch2 v2
 public class Class3 {
     public static void main(String[] args) {
-        System.out.println("Class3 v1");
+        System.out.println("Class3 v2 in branch2");
     }
 }
