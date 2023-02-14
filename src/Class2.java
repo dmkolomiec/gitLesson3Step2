a6b58297f51423d720f2c0a5643164c1cfa75eed
dmKolomiec <dmkolomiec@gmail.com>
2023-02-14 18:28:34 +0200

    Changed branch to 'branch1' and ch. Class2.java

--- src/Class2.java
+++ src/Class2.java
@@ -1,5 +1,6 @@
+// branch1
 public class Class2 {
     public static void main(String[] args) {
-        System.out.println("Class2 v1");
+        System.out.println("Class2 v2 (branch1)");
     }
 }
